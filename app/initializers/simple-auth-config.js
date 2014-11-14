254b1794bd1f8cfec25c58d921ea8925e124b524
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-11-14 10:27:01 +0100

    removed unnecessary initializer

--- app/initializers/simple-auth-config.js
+++
@@ -1,9 +0,0 @@
-/* global EmberSimpleAuthSampleENV */
-
-export default {
-  name:       'simple-auth-config',
-  before:     'simple-auth',
-  initialize: function() {
-    window.ENV = EmberSimpleAuthSampleENV;
-  }
-};
