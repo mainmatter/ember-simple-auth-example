5e7e2a3146856fcc707c6cc22124a437beaa9807
Jimmy Beaudoin <contact@jim-beaudoin.com>
2015-10-24 00:54:41 -0400

    Add create session action with json response

--- devise-backend/app/controllers/sessions_controller.rb
+++ devise-backend/app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
-class SessionsController < ApplicationController
+class SessionsController < Devise::SessionsController
+  skip_before_action :authenticate_user_from_token!
+  skip_before_action :verify_authenticity_token
+  def create
+    respond_to do |format|
+      format.html { super }
+      format.json do
+        self.resource = warden.authenticate!(auth_options)
+        sign_in(resource_name, resource)
+        data = {
+          user_token: self.resource.authentication_token,
+          user_email: self.resource.email
+        }
+        render json: data, status: 201
+      end
+    end
+  end
 end
